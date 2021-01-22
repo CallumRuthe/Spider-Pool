ad412e9a2af9fe6a50712ab414f5cb019f4d6b99
Callum Ruthe <CR000052@sd38.bc.ca>
2021-01-22 11:37:57 -0800

    Finished enemy and money class.

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 # My final project for programming 2
 
 import pygame
-
+import random
+# TODO: Play testing / debugging
 # ----- CONSTANTS
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -14,11 +15,14 @@ JUMP_VEL = 10
 MAX_JUMPS = 2
 PROJECTILE_SPEED = 10
 MAX_PROJECTILES = 5
-MAX_COINS = 50
+MONEY_GOAL = 50
+MONEY_VEL = -5
+MONEY_CHANCE = 1000
+ENEMY_CHANCE = 1250
+ENEMY_VEL = -5
 TITLE = "SPIDER-POOL"
 
 
-# TODO: PLayer class
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -32,7 +36,6 @@ class Player(pygame.sprite.Sprite):
         self.y_vel = 0
         self.num_jumps = 0
 
-    # Movement -> Vertical only
     def update(self):
         """ Move the player"""
         self.calc_grav()
@@ -58,14 +61,86 @@ class Player(pygame.sprite.Sprite):
             self.num_jumps += 1
 
 
-# TODO: Enemy class
 # randomly spawning enemies
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
 
-# TODO: pickups classes
-# refill ammo
-# collect coins to win
+        self.image = pygame.image.load("Images/polverine.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = WIDTH + 10
+        self.rect.y = random.randrange(0, HEIGHT - self.rect.height)
 
-# TODO: Projectile class
+        self.x_vel = ENEMY_VEL
+
+    def update(self):
+        self.rect.x += self.x_vel
+
+
+# collect money to win
+class Money(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.animation = [
+            pygame.image.load("Images/Money Animation/flyingMoney-1.png"),
+            pygame.image.load("Images/Money Animation/flyingMoney-2.png"),
+            pygame.image.load("Images/Money Animation/flyingMoney-3.png"),
+            pygame.image.load("Images/Money Animation/flyingMoney-4.png"),
+            pygame.image.load("Images/Money Animation/flyingMoney-5.png"),
+            pygame.image.load("Images/Money Animation/flyingMoney-6.png"),
+            pygame.image.load("Images/Money Animation/flyingMoney-7.png"),
+            pygame.image.load("Images/Money Animation/flyingMoney-8.png"),
+        ]
+
+        self.animation_frame = 0
+
+        self.image = self.animation[0]
+        self.image = pygame.transform.scale(self.image, (100, 91))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = WIDTH + 10
+        self.rect.y = random.randrange(0, HEIGHT - self.rect.height)
+
+        self.x_vel = MONEY_VEL
+
+    def update(self):
+        self.animate()
+        self.rect.x += self.x_vel
+
+        if self.rect.right <= 0:
+            self.kill()
+
+    def animate(self):
+        if self.animation_frame % 8 == 0:
+            self.image = self.animation[0]
+            self.image = pygame.transform.scale(self.image, (100, 91))
+        elif self.animation_frame % 8 == 1:
+            self.image = self.animation[1]
+            self.image = pygame.transform.scale(self.image, (100, 91))
+        elif self.animation_frame % 8 == 2:
+            self.image = self.animation[2]
+            self.image = pygame.transform.scale(self.image, (100, 91))
+        elif self.animation_frame % 8 == 3:
+            self.image = self.animation[3]
+            self.image = pygame.transform.scale(self.image, (100, 91))
+        elif self.animation_frame % 8 == 4:
+            self.image = self.animation[4]
+            self.image = pygame.transform.scale(self.image, (100, 91))
+        elif self.animation_frame % 8 == 5:
+            self.image = self.animation[5]
+            self.image = pygame.transform.scale(self.image, (100, 91))
+        elif self.animation_frame % 8 == 6:
+            self.image = self.animation[6]
+            self.image = pygame.transform.scale(self.image, (100, 91))
+        elif self.animation_frame % 8 == 7:
+            self.image = self.animation[7]
+            self.image = pygame.transform.scale(self.image, (100, 91))
+
+        self.animation_frame += 1
+
+
+# create shuriken projectiles
 class Projectile(pygame.sprite.Sprite):
     def __init__(self, coords):
         """
@@ -74,7 +149,13 @@ class Projectile(pygame.sprite.Sprite):
         """
         super().__init__()
 
-        self.image = pygame.image.load("Images/Shuriken Animation/shuriken.png")
+        self.animation = [
+            pygame.image.load("Images/Shuriken Animation/shuriken.png"),
+            pygame.image.load("Images/Shuriken Animation/shuriken-2.png")
+        ]
+        self.animation_frame = 0
+
+        self.image = self.animation[0]
         self.rect = self.image.get_rect()
         self.rect.centerx, self.rect.centery = coords
 
@@ -84,13 +165,17 @@ class Projectile(pygame.sprite.Sprite):
         self.rect.x += self.x_vel
         self.animate()
 
-    # TODO: animate shuriken
+        if self.rect.left >= WIDTH:
+            self.kill()
+
     def animate(self):
         """ create the animation of the projectile"""
-        if self.image == pygame.image.load("Images/Shuriken Animation/shuriken-2.png"):
-            self.image = pygame.image.load("Images/Shuriken Animation/shuriken.png")
-        elif self.image == pygame.image.load("Images/Shuriken Animation/shuriken.png"):
-            self.image = pygame.image.load("Images/Shuriken Animation/shuriken-2.png")
+        if self.animation_frame % 2 == 0:
+            self.image = self.animation[0]
+        else:
+            self.image = self.animation[1]
+
+        self.animation_frame += 1
 
 
 def main():
@@ -104,15 +189,23 @@ def main():
     # ----- LOCAL VARIABLES
     done = False
     clock = pygame.time.Clock()
+    money_count = 0
+
+    # text = pygame.font.SysFont("arial", True)
+    # write = "jbafbjkaf"
 
     # Sprite groups
     all_sprites = pygame.sprite.Group()
     projectile_sprites = pygame.sprite.Group()
+    enemy_sprites = pygame.sprite.Group()
+    player_sprite = pygame.sprite.Group()
+    money_sprites = pygame.sprite.Group()
 
     # Sprites
     # Player sprite
     player = Player()
     all_sprites.add(player)
+    player_sprite.add(player)
 
     # ----- MAIN LOOP
     while not done:
@@ -121,22 +214,76 @@ def main():
             if event.type == pygame.QUIT:
                 done = True
 
-            # TODO: Keyboard / mouse controls
+            # Player jump on space bar press
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     player.jump()
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            # player shoot on mouse click
+            if event.type == pygame.MOUSEBUTTONDOWN and len(projectile_sprites) <= 5:
                 shuriken = Projectile((player.rect.right, player.rect.centery))
                 all_sprites.add(shuriken)
                 projectile_sprites.add(shuriken)
 
+            # WIN condition: reached coin goal
+            if money_count >= MONEY_GOAL:
+                done = True
+                print("you win")
+
         # ----- LOGIC
         all_sprites.update()
+
+
+        # spawn money
+        money_spawn_chance = random.randrange(0, MONEY_CHANCE)
+        if money_spawn_chance < 10:
+            money = Money()
+            all_sprites.add(money)
+            money_sprites.add(money)
+
+        # spawn enemies
+        enemy_spawn_chance = random.randrange(0, ENEMY_CHANCE)
+        if enemy_spawn_chance < 10:
+            enemy = Enemy()
+            all_sprites.add(enemy)
+            enemy_sprites.add(enemy)
+
+        # --- Collision
+        # bullet hits enemy
+        for shuriken in projectile_sprites:
+            enemy_hit = pygame.sprite.spritecollide(shuriken, enemy_sprites, True)
+            if len(enemy_hit) > 0:
+                shuriken.kill()
+
+        # enemy hits player
+        for enemy in enemy_sprites:
+            player_hit = pygame.sprite.spritecollide(enemy, player_sprite, True)
+            if len(player_hit) > 0:
+                done = True
+                print("you lose")
+
+        # player collects money
+        money_hit = pygame.sprite.spritecollide(player, money_sprites, True)
+        for money in money_hit:
+            money_count += 1
+
+        # TODO: secondary loss option if too many enemies get past player
 
         # ----- DRAW
         screen.fill(SKY_BLUE)
         all_sprites.draw(screen)
+
+        # img = text.render(write, True, BLACK)
+        # screen.blit(img, (100, 100))
+
+        # TODO: counter for money
+        # TODO: counter for ammo
+
+        # TODO: Win screen
+        # TODO: Lose screen
+
+        # TODO: Title screen (optional)
+        # TODO: Background (optional)
 
         # ----- UPDATE
         pygame.display.flip()
